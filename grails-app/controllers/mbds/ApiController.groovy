1985d8f77bcc025880020cf5cdca5007d346ff27
SergioSim <sergio.simonian@etu.unice.fr>
2018-09-25 13:32:34 +0200

    getUser Method in ApiController

--- grails-app/controllers/mbds/ApiController.groovy
+++ grails-app/controllers/mbds/ApiController.groovy
@@ -1,8 +1,14 @@
 package mbds
+import grails.converters.JSON
 
 class ApiController {
 
     def index() {
         render  "ok"
+    }
+
+    def getUser() {
+        def converter = new JSON(target: User.list());
+        render converter
     }
 }
