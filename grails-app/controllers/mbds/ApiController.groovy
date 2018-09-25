2b995d86039666f3c8fc61afe2483260bdb5fe2a
Nadir <Nadir>
2018-09-25 15:30:23 +0200

    Ajout du verbe http POST sur l'entité User'

--- grails-app/controllers/mbds/ApiController.groovy
+++ grails-app/controllers/mbds/ApiController.groovy
@@ -11,9 +11,16 @@ class ApiController {
                 def rep= new JSON(target:User.list())
                 render rep
                 break;
-            case "POST":
-                def user = new User(request).save(flush: true)
 
+            case "POST":
+                println(request.JSON)
+                if(new User(request.JSON).save(flush:true)){
+                    response.status=200
+                }
+                else {
+                    response.status=300
+                }
+                break;
             default:
                 response: 500;
         }
