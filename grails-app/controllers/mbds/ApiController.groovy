26ed58d5c1c4a3d91c68a966a65e9f6ba69a9f5f
SergioSim <sergio.simonian@etu.unice.fr>
2018-09-25 16:03:53 +0200

    add get user by id

--- grails-app/controllers/mbds/ApiController.groovy
+++ grails-app/controllers/mbds/ApiController.groovy
@@ -3,26 +3,35 @@ import grails.converters.JSON;
 class ApiController {
 
     def index() {
-        render text:"Ohhh";
-}
-    def user(){
-        switch (request.getMethod()) {
-            case "GET":
-                def rep= new JSON(target:User.list())
-                render rep
-                break;
+        render text:"Ohhh"
+    }
 
-            case "POST":
-                println(request.JSON)
-                if(new User(request.JSON).save(flush:true)){
-                    response.status=200
-                }
-                else {
-                    response.status=300
-                }
-                break;
-            default:
-                response: 500;
+    def user(long id) {
+        if (id != 0) {
+            def user = User.findById(id)
+            if (user == null) {
+                render(status: 400, text: "User not found")
+            } else {
+                render new JSON(target: user)
+            }
+        } else {
+            switch (request.getMethod()) {
+                case "GET":
+                    def rep = new JSON(target: User.list())
+                    render rep
+                    break
+
+                case "POST":
+                    println(request.JSON)
+                    if (new User(request.JSON).save(flush: true)) {
+                        response.status = 200
+                    } else {
+                        response.status = 300
+                    }
+                    break
+                default:
+                    render(status: 500, text: "User not found")
+            }
         }
     }
 }
