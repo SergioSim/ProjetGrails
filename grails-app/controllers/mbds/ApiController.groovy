b15c4d8af70f5bd21a814faf9114ab29c4837de4
SergioSim <sergio.simonian@etu.unice.fr>
2018-09-25 22:14:46 +0200

    replace findById with get

--- grails-app/controllers/mbds/ApiController.groovy
+++ grails-app/controllers/mbds/ApiController.groovy
@@ -1,37 +1,38 @@
 package mbds
-import grails.converters.JSON;
+import grails.converters.JSON
+
 class ApiController {
 
     def index() {
         render text:"Ohhh"
     }
 
-    def user(long id) {
-        if (id != 0) {
-            def user = User.findById(id)
-            if (user == null) {
-                render(status: 400, text: "User not found")
-            } else {
-                render new JSON(target: user)
-            }
-        } else {
-            switch (request.getMethod()) {
-                case "GET":
-                    def rep = new JSON(target: User.list())
-                    render rep
-                    break
-
-                case "POST":
-                    println(request.JSON)
-                    if (new User(request.JSON).save(flush: true)) {
-                        response.status = 200
+    def user(String id) {
+        switch (request.getMethod()) {
+            case "GET":
+                if (id != 0 && id != null) {
+                    def user = User.get(id)
+                    if (user == null) {
+                        render(status: 400, text: "User not found")
                     } else {
-                        response.status = 300
+                        render new JSON(target: user)
                     }
                     break
-                default:
-                    render(status: 500, text: "User not found")
-            }
+                }
+                def rep = new JSON(target: User.list())
+                render rep
+                break
+
+            case "POST":
+                println(request.JSON)
+                if (new User(request.JSON).save(flush: true)) {
+                    response.status = 201
+                } else {
+                    response.status = 300
+                }
+                break
+            default:
+                render(status: 500, text: "User not found")
         }
     }
 }
